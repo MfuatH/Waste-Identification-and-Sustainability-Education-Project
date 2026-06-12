4b69eb9222d5a8d1c2b9a089d07ff052722ce926
Raditya Bani Ainur Ridho <149133820+rradz11@users.noreply.github.com>
2026-06-12 21:56:42 +0700

    Update navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,10 @@
-<header class="bg-white border-b sticky top-0 z-40">
+<header class="bg-white border-b border-slate-300 sticky top-0 z-40">
 
     <div class="h-20 px-6 flex items-center justify-between">
 
         <div class="flex items-center gap-4">
 
-            <button id="sidebarToggle" class="lg:hidden p-3 rounded-2xl bg-slate-100 text-slate-600 hover:bg-slate-200 transition">
+            <button id="sidebarToggle" class="p-3 rounded-2xl bg-slate-100 text-slate-600 hover:bg-slate-200 transition">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
