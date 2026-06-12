9e319986bdf034922a8935ffc208a016e4f00bdf
Raditya Bani Ainur Ridho <149133820+rradz11@users.noreply.github.com>
2026-06-12 20:18:18 +0700

    Filter data dashboard by user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,28 +7,34 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $totalScans = Classification::count();
+        $totalScans = Classification::where('user_id', auth()->id())->count();
 
-        $plastic = Classification::where(
+        $plastic = Classification::where('user_id', auth()->id())->where(
             'category',
             'Plastic Waste'
         )->count();
 
-        $organic = Classification::where(
+        $organic = Classification::where('user_id', auth()->id())->where(
             'category',
             'Organic Waste'
         )->count();
 
-        $ewaste = Classification::where(
+        $ewaste = Classification::where('user_id', auth()->id())->where(
             'category',
             'E-Waste'
         )->count();
+
+        $recentActivities = Classification::where('user_id', auth()->id())
+            ->latest()
+            ->take(5)
+            ->get();
 
         return view('dashboard.index',compact(
             'totalScans',
             'plastic',
             'organic',
-            'ewaste'
+            'ewaste',
+            'recentActivities'
         ));
     }
 }
